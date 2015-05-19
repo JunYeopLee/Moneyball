99569bc6eb84b65dec1ef8eadc724aaca4b0f30e
sjw <jwseo900504@gmail.com>
2015-05-19 17:37:17 +0900

    Login, Signin activity 생성 -지우

--- app/src/main/java/com/example/junyeop_imaciislab/moneyball/Moneyball/MainActivity.java
+++ app/src/main/java/com/example/junyeop_imaciislab/moneyball/Moneyball/MainActivity.java
@@ -27,14 +27,16 @@ public class MainActivity extends SampleActivityBase {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
             SlidingTabsBasicFragment fragment = new SlidingTabsBasicFragment();
             transaction.replace(R.id.sample_content_fragment, fragment);
             transaction.commit();
-        }
+
+
     }
 
     @Override
