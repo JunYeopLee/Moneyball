ab319476e7bfc6b895c193df8ac55a713820df66
JunYeopLee <duql2072@gmail.com>
2015-07-21 00:42:50 +0900

    signup 아이디 수정 & SignupActivity OnclickListener

--- app/src/main/java/com/example/junyeop_imaciislab/moneyball/Moneyball/SignupActivity.java
+++ app/src/main/java/com/example/junyeop_imaciislab/moneyball/Moneyball/SignupActivity.java
@@ -4,15 +4,39 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.junyeop_imaciislab.moneyball.R;
 
 public class SignupActivity extends Activity {
 
+    private EditText idEdit;
+    private EditText pwEdit;
+    private EditText pwcEdit;
+    private Button signUpbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        idEdit = (EditText)findViewById(R.id.id_edit);
+        pwEdit = (EditText)findViewById(R.id.pw_edit);
+        pwcEdit = (EditText)findViewById(R.id.pwcheck_edit);
+        signUpbtn = (Button)findViewById(R.id.btn_signup_insignup);
+        signUpbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String ids = idEdit.getText().toString();
+                String pws = pwEdit.getText().toString();
+                String pwcs = pwcEdit.getText().toString();
+                if( pws.compareTo(pwcs) != 0 ) { // Mismatch
+                    // TODO : 비밀번호체크 경고창
+                } else { // Accepted
+                    // TODO : 서버로 쿼리
+                }
+            }
+        });
     }
 
     @Override
