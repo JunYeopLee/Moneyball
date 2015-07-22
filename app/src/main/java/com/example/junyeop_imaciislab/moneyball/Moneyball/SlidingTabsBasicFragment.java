c3b918b6db0976ac3fd1b6c879525779ffaf6fb6
junyeop_imaciislab <duql2072@gmail.com>
2015-07-22 18:29:17 +0900

    회원가입 완성

--- app/src/main/java/com/example/junyeop_imaciislab/moneyball/Moneyball/SlidingTabsBasicFragment.java
+++ app/src/main/java/com/example/junyeop_imaciislab/moneyball/Moneyball/SlidingTabsBasicFragment.java
@@ -48,18 +48,30 @@ import com.example.junyeop_imaciislab.moneyball.common.view.SlidingTabLayout;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
+import org.apache.http.HttpVersion;
 import org.apache.http.StatusLine;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
+import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.params.HttpProtocolParams;
+import org.apache.http.protocol.HTTP;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.security.KeyStore;
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -399,7 +411,7 @@ public class SlidingTabsBasicFragment extends Fragment {
         @Override
         protected HttpResponse doInBackground(String... urls) {
             HttpResponse response = null;
-            HttpClient client = new DefaultHttpClient();
+            HttpClient client = getHttpClient();
             HttpConnectionParams.setConnectionTimeout(client.getParams(), 10000);
             HttpGet httpGet = new HttpGet(urls[0]);
             try {
@@ -469,6 +481,35 @@ public class SlidingTabsBasicFragment extends Fragment {
         @Override
         protected void onPostExecute(HttpResponse response) {
             // Progreebar hide
+        }
+
+        /**
+         *
+         * For HTTPS protocol
+         *
+         * */
+        private HttpClient getHttpClient() {
+            try {
+                KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+                trustStore.load(null, null);
+
+                SSLSocketFactory sf = new SFSSLSocketFactory(trustStore);
+                sf.setHostnameVerifier(SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+
+                HttpParams params = new BasicHttpParams();
+                HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
+                HttpProtocolParams.setContentCharset(params, HTTP.UTF_8);
+
+                SchemeRegistry registry = new SchemeRegistry();
+                registry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
+                registry.register(new Scheme("https", sf, 443));
+
+                ClientConnectionManager ccm = new ThreadSafeClientConnManager(params, registry);
+
+                return new DefaultHttpClient(ccm, params);
+            } catch (Exception e) {
+                return new DefaultHttpClient();
+            }
         }
     }
 }
